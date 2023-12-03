62b3f21ae3fc48872ef5cca8e1671fd9356d77d9
amirhossein <amirgolami131385@gmail.com>
2023-12-03 20:08:43 +0330

    add custom admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as UserBaseAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from accounts.models import User
+from accounts.forms import UserCreationForm, UserChangeForm
+
+
+class UserAdmin(UserBaseAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'phone_number', 'is_admin')
+    list_filter = ('is_admin',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'phone_number', 'full_name')}),
+        ("permission", {'fields': ('is_admin', 'is_active', 'last_login')}),
+    )
+
+    add_fieldsets = (
+        (None, {'fields': ('email', 'phone_number', 'full_name', 'password1', 'password2')}),
+    )
+    search_fields = ('email', 'full_name')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
+
--- accounts/models.py
+++ accounts/models.py
@@ -7,14 +7,14 @@ from accounts import managers
 class User(AbstractBaseUser):
     email = models.EmailField(max_length=255, unique=True)
     phone_number = models.CharField(max_length=11, unique=True)
-    full_name = models.CharField(max_length=200)
+    full_name = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
     objects = managers.UserManager()
 
     USERNAME_FIELD = 'phone_number'
-    REQUIRED_FIELDS = ['email', 'fullname']
+    REQUIRED_FIELDS = ['email', 'full_name']
 
     def __str__(self):
         return self.email
